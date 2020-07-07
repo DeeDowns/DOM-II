e84d6a736fa062070b0f18cbf40e093181a57736
Dee <47228065+DeeDowns@users.noreply.github.com>
2020-07-07 18:28:10 -0400

    completed stopPropagation and preventDefault problems

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 // Your code goes here
+
+//1.mouseenter 
+//2.mouseleave
 const navLinks = document.querySelectorAll('.nav-link')
 console.log(navLinks)
 
@@ -11,6 +14,8 @@ navLinks.forEach(item => {
     })
 })
 
+
+//3. mousemove
 const logoHeading = document.querySelector('.logo-heading');
 console.log(logoHeading)
 
@@ -18,6 +23,9 @@ logoHeading.addEventListener('mousemove', () => {
     logoHeading.style.backgroundColor = 'orange'
 })
 
+
+//4.dblclick
+//5.click
 const button = document.querySelectorAll('.btn');
 console.log(button)
 
@@ -30,6 +38,8 @@ button.forEach(item => {
     })
 })
 
+
+//6.keydown
 const hTwoHeadings = document.querySelectorAll('h2')
 console.log(hTwoHeadings)
 
@@ -41,6 +51,7 @@ document.addEventListener('keydown', (e) => {
     }
 })
 
+//7.keyup
 document.addEventListener('keyup', (e) => {
     if(e.key === 'ArrowUp' || e.key === 'ArrowDown'){
       hTwoHeadings.forEach(item => {
@@ -49,6 +60,8 @@ document.addEventListener('keyup', (e) => {
     }
 })
 
+
+//8.scroll
 const header = document.querySelector('.main-navigation')
 console.log(header)
 
@@ -56,25 +69,63 @@ document.addEventListener('scroll', () => {
    header.style.backgroundColor = 'grey'
 })
 
-
+//9.drag
 const destinationHTwo = document.querySelector('.content-destination h2');
 const destinationImage = document.querySelector('.content-destination img');
-
 
 destinationImage.addEventListener('drag', () => {
     destinationHTwo.textContent = 'whoa there'
 })
 
+//10.dragend
 destinationImage.addEventListener('dragend', () => {
     destinationHTwo.textContent = 'Thats better, now pick a destination'
 })
 
 
+// Prevent event propogation of nested elements
+const textContentP = document.querySelectorAll('.text-content p')
+console.log('text p list', textContentP)
+const textContentP3 = textContentP[2]
+console.log('text p ', textContentP3)
+const textContent = document.querySelectorAll('.text-content')
+console.log('text box list',textContent)
+const textContent2 = textContent[1]
+console.log('text box', textContent2)
+
+textContentP3.addEventListener('click', (e) => {
+    e.target.style.backgroundColor = 'orange'
+})
+
+textContent2.addEventListener('mouseenter', (e) => {
+    e.target.style.border = '3px solid blue';
+    e.target.style.backgroundColor = 'green'
+})
+
+textContent2.addEventListener('mouseleave', (e) => {
+    e.target.style.border = '';
+    e.target.style.backgroundColor = ''
+})
+
+textContent2.addEventListener('click', (e) => {
+    e.target.style.backgroundColor = 'red';
+})
+
+textContentP3.addEventListener('click', (e) => {
+    e.stopPropagation();
+})
 
 
-// window.addEventListener('load', () => {
-//     alert('running out of ideas for event listeners lol')
-// })
+
+
+
+//Stop nav items from refreshing page
+navLinks.forEach(item => {
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
+    })
+})
+
 
 
 
